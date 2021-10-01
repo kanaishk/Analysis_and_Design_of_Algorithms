b655cca16ef8e671362eb3f59b9f62623e3e9c38
Kanaishk Garg <kanaishkgarg@gmail.com>
2021-10-01 10:08:10 +0530

    Added Merge Sort and moved the array creation function to the class.

--- SortTimeComplexity.cpp
+++ SortTimeComplexity.cpp
@@ -10,15 +10,14 @@ sorting algorithms. It will show time result for best, worst and average case.
 using namespace std;
 using namespace std::chrono;
 
-int* bestarr(int n);
-int* worstarr(int n);
-int* averagearr(int n);
 void swap(int *xp, int *yp);
 void bubblesort(int arr[], int n);
 void insertionsort(int arr[], int n);
 void selectionsort(int arr[], int n);
 int partition(int arr[], int low, int high);
 void quicksort(int arr[], int low, int high);
+void merge(int array[], int const left, int const mid, int const right);
+void mergesort(int array[], int const begin, int const end);
 void sortfunc(int arr[], int n, char ch='b');
 
 class complexity
@@ -28,6 +27,48 @@ class complexity
         double best;
         double worst;
         double average;
+        int* bestarr(int n)
+        {
+            int* arr=new int[n];
+            if(!arr)
+            {
+                cout << "Cannot initialize memory";
+                return NULL;
+            }
+            for(int i=0; i<n; i++)
+            {
+                arr[i]=i;
+            }
+            return arr;
+        }
+        int* worstarr(int n)
+        {
+            int* arr=new int[n];
+            if(!arr)
+            {
+                cout << "Cannot initialize memory";
+                return NULL;
+            }
+            for(int i=0; i<n; i++)
+            {
+                arr[i]=n-i;
+            }
+            return arr;
+        }
+        int* averagearr(int n)
+        {
+            int* arr=new int[n];
+            if(!arr)
+            {
+                cout << "Cannot initialize memory";
+                return NULL;
+            }
+            for(int i=0; i<n; i++)
+            {
+                arr[i]=rand()%n;
+            }
+            return arr;
+        }
         double return_dur(int* arr, int n, char ch)
         {
             // Get starting timepoint
@@ -97,7 +138,7 @@ class complexity
 int main()
 {
     complexity c1(1000), c2(5000), c3(10000), c4(20000);
-    int ch=5;
+    int ch=6;
     do
     {
         system("cls");
@@ -107,8 +148,9 @@ int main()
         cout << " 2. Insertion Sort\n";
         cout << " 3. Selection Sort\n";
         cout << " 4. Quick Sort\n";
-        cout << " 5. Exit\n";
-        cout << "Enter your choice(1-5): ";
+        cout << " 5. Merge Sort\n";
+        cout << " 6. Exit\n";
+        cout << "Enter your choice(1-6): ";
         cin >> ch;
         system("cls");
         switch(ch)
@@ -133,58 +175,18 @@ int main()
                     c3.display('q');
                     c4.display('q');
                     break;
-            case 5: break;
+            case 5: c1.display('m');
+                    c2.display('m');
+                    c3.display('m');
+                    c4.display('m');
+                    break;
+            case 6: break;
             default:cout << "Try Again...!";
         }
-        cout << "\n\n";
+        cout << "\n";
         system("pause");
-    }while(ch!=5);
+    }while(ch!=6);
     return 0;
-}
-
-int* bestarr(int n)
-{
-    int* arr=new int[n];
-    if(!arr)
-    {
-        cout << "Cannot initialize memory";
-        return NULL;
-    }
-    for(int i=0; i<n; i++)
-    {
-        arr[i]=i;
-    }
-    return arr;
-}
-
-int* worstarr(int n)
-{
-    int* arr=new int[n];
-    if(!arr)
-    {
-        cout << "Cannot initialize memory";
-        return NULL;
-    }
-    for(int i=0; i<n; i++)
-    {
-        arr[i]=n-i;
-    }
-    return arr;
-}
-
-int* averagearr(int n)
-{
-    int* arr=new int[n];
-    if(!arr)
-    {
-        cout << "Cannot initialize memory";
-        return NULL;
-    }
-    for(int i=0; i<n; i++)
-    {
-        arr[i]=rand()%n;
-    }
-    return arr;
 }
 
 void swap(int *xp, int *yp)
@@ -258,6 +260,62 @@ void quicksort(int arr[], int low, int high)
     }
 }
 
+void merge(int array[], int const left, int const mid, int const right)
+{
+    auto const subArrayOne = mid - left + 1;
+    auto const subArrayTwo = right - mid;
+
+    auto *leftArray = new int[subArrayOne], *rightArray = new int[subArrayTwo];
+
+    for (auto i = 0; i < subArrayOne; i++)
+        leftArray[i] = array[left + i];
+    for (auto j = 0; j < subArrayTwo; j++)
+        rightArray[j] = array[mid + 1 + j];
+
+    auto indexOfSubArrayOne = 0, indexOfSubArrayTwo = 0;
+    int indexOfMergedArray = left;
+
+    while (indexOfSubArrayOne < subArrayOne && indexOfSubArrayTwo < subArrayTwo)
+    {
+        if (leftArray[indexOfSubArrayOne] <= rightArray[indexOfSubArrayTwo])
+        {
+            array[indexOfMergedArray] = leftArray[indexOfSubArrayOne];
+            indexOfSubArrayOne++;
+        }
+        else
+        {
+            array[indexOfMergedArray] = rightArray[indexOfSubArrayTwo];
+            indexOfSubArrayTwo++;
+        }
+        indexOfMergedArray++;
+    }
+
+    while (indexOfSubArrayOne < subArrayOne)
+        {
+        array[indexOfMergedArray] = leftArray[indexOfSubArrayOne];
+        indexOfSubArrayOne++;
+        indexOfMergedArray++;
+    }
+
+    while (indexOfSubArrayTwo < subArrayTwo)
+    {
+        array[indexOfMergedArray] = rightArray[indexOfSubArrayTwo];
+        indexOfSubArrayTwo++;
+        indexOfMergedArray++;
+    }
+}
+
+void mergesort(int array[], int const begin, int const end)
+{
+    if (begin >= end)
+        return;
+
+    auto mid = begin + (end - begin) / 2;
+    mergesort(array, begin, mid);
+    mergesort(array, mid + 1, end);
+    merge(array, begin, mid, end);
+}
+
 void sortfunc(int arr[], int n, char ch)
 {
     switch(ch)
@@ -269,6 +327,8 @@ void sortfunc(int arr[], int n, char ch)
         case 's':   selectionsort(arr,n);
                     break;
         case 'q':   quicksort(arr,0,n-1);
+                    break;
+        case 'm':   mergesort(arr,0,n-1);
                     break;
     }
 }
